625da17c752c2ba1f862fda3672cfc68d826dc5b
Juan David <JD_20rb@hotmail.com>
2023-03-15 22:34:18 +0100

    ci: fix backend cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://192.168.0.168:8080',
+    origin: 'http://localhost:8080',
     methods: 'GET,POST,PUT,PATCH,HEAD,DELETE',
   });
   await app.listen(3001);
